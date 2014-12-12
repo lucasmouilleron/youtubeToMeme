c2fd79548f0248c8b7c7f57c8c715d46589efe09
Lucas Mouilleron <LucasMouileron>
2014-12-12 16:27:34 +0100

    contents

--- meme.php
+++ meme.php
@@ -6,10 +6,18 @@ define("POSITION_TOP",2); //Imagick::GRAVITY_NORTH
 define("POSITION_BOTTOM",8); //Imagick::GRAVITY_SOUTH
 
 ///////////////////////////////////////////////////////////////////
+
 $meme = ["A7TaY8HWYd8",71 ,5, "PRETTY GOOD JUNIOR"];
 //$meme = ["6Hn8qnsucwo",30,5, "PRETTY ... PRETTY ... PRETTY GOOD !"];
 //$meme = ["_tj5ye2r8RI",30,5, "PRETTY ... PRETTY ... PRETTY GOOD !"];
 //$meme = ["yLAjKtmT3lk",30,5, "PRETTY ... PRETTY ... PRETTY ... PRETTY ... PRETTY GOOD !"];
+//$meme = ["eL2I5GY_Vvs",166 ,5, "T'ES SERIEUX GRENOBLE"];
+//$meme = ["vuR1Aous22o",130 ,5, "SANDRA WTF ???"];
+//$meme = ["xaZK_2m3NjU",180 ,2, "CAN'T WAIT FOR THE NEXT ONE"];
+//$meme = ["QJbZ-IT1gU8",21 ,5, "T'ES SERIEUX LE DRIFT"];
+//$meme = ["-onuvbdl23Q",64 ,4, "SOUVENIR DE SOIREE AVEC TON AMI LUCAS. BISOUS MON MAXIME. TA MAMAN"];
+
+
 $width = 500;
 $fontSize = 40;
 $color = "white";
